9a155a39498e05cd5049a90db438501ff9c90518
Jorbon <ben.a.phillips@outlook.com>
2023-12-13 13:31:29 -0600

    more features

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,23 @@
 use std::{os::{raw::c_void, windows::ffi::OsStrExt}, ffi::OsStr};
-use windows::{core::{Result, PCWSTR, Error, HSTRING}, Win32::{Foundation::{HWND, RECT, LPARAM, LRESULT, WPARAM}, UI::WindowsAndMessaging::{self, CS_HREDRAW, CS_VREDRAW, WS_EX_TOPMOST, WS_OVERLAPPEDWINDOW, HICON, RegisterClassW, LoadCursorW, WNDCLASSW, IDC_ARROW, DefWindowProcW, GetWindowLongPtrW, SetWindowLongPtrW, WM_NCCREATE, CREATESTRUCTW, GWLP_USERDATA, TranslateMessage, DispatchMessageW, GetMessageW, PostQuitMessage, MSG, CreateWindowExW, CW_USEDEFAULT, SW_SHOW, ShowWindow, GetClientRect, WINDOW_EX_STYLE, CreateMenu, MF_STRING, AppendMenuW, SetMenu, MF_POPUP, AdjustWindowRectEx, SetTimer, KillTimer}, System::{WinRT::{DispatcherQueueOptions, RoInitialize, DQTYPE_THREAD_CURRENT, DQTAT_COM_NONE, RO_INIT_SINGLETHREADED, CreateDispatcherQueueController}, LibraryLoader::GetModuleHandleW}, Graphics::Gdi::{PAINTSTRUCT, BeginPaint, EndPaint, SelectObject, DeleteObject, CreateCompatibleDC, BITMAPINFO, BITMAPINFOHEADER, RGBQUAD, BI_RGB, CreateDIBSection, DIB_RGB_COLORS, BitBlt, SRCCOPY, DeleteDC, HBRUSH, HBITMAP, GetDC, ReleaseDC, InvalidateRect}}, Foundation::AsyncActionCompletedHandler};
+use windows::{core::{Result, PCWSTR, Error, HSTRING}, Win32::{Foundation::{HWND, RECT, LPARAM, LRESULT, WPARAM}, UI::WindowsAndMessaging::{self, CS_HREDRAW, CS_VREDRAW, WS_EX_TOPMOST, WS_OVERLAPPEDWINDOW, HICON, RegisterClassW, LoadCursorW, WNDCLASSW, IDC_ARROW, DefWindowProcW, GetWindowLongPtrW, SetWindowLongPtrW, WM_NCCREATE, CREATESTRUCTW, GWLP_USERDATA, TranslateMessage, DispatchMessageW, GetMessageW, PostQuitMessage, MSG, CreateWindowExW, CW_USEDEFAULT, SW_SHOW, ShowWindow, GetClientRect, WINDOW_EX_STYLE, /*CreateMenu, MF_STRING, AppendMenuW, SetMenu, MF_POPUP,*/ AdjustWindowRectEx, SetTimer, KillTimer}, System::{WinRT::{DispatcherQueueOptions, RoInitialize, DQTYPE_THREAD_CURRENT, DQTAT_COM_NONE, RO_INIT_SINGLETHREADED, CreateDispatcherQueueController}, LibraryLoader::GetModuleHandleW}, Graphics::Gdi::{PAINTSTRUCT, BeginPaint, EndPaint, SelectObject, DeleteObject, CreateCompatibleDC, BITMAPINFO, BITMAPINFOHEADER, RGBQUAD, BI_RGB, CreateDIBSection, DIB_RGB_COLORS, BitBlt, SRCCOPY, DeleteDC, HBRUSH, HBITMAP, GetDC, ReleaseDC, InvalidateRect}}, Foundation::AsyncActionCompletedHandler};
 
 mod tests;
 
 
 // Todo:
-// port timer as a settimeout function
-// replace unwraps with early error
-// fix up size and sizing handlers
 // menu builder
 // more handles
 
 
-pub type Rect = RECT;
-trait SimpleWindowsRect {
-	fn width(&self) -> i32;
-	fn height(&self) -> i32;
+#[repr(C)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Default)]
+pub struct Rect {
+    pub left: i32,
+    pub top: i32,
+    pub right: i32,
+    pub bottom: i32
 }
-impl SimpleWindowsRect for Rect {
+impl Rect {
 	fn width(&self) -> i32 {
 		self.right - self.left
 	}
@@ -29,28 +29,42 @@ impl SimpleWindowsRect for Rect {
 
 pub trait SimpleWindowApp {
 	#[allow(unused_variables)]
-	fn on_paint(&mut self, pixel_buffer: &mut [u8], client_rect: &Rect) {}
+	fn on_paint(&mut self, handle: &WindowHandle, pixel_buffer: &mut [u8], client_rect: &Rect) {}
 	#[allow(unused_variables)]
-	fn on_command(&mut self, pixel_buffer: &mut [u8], client_rect: &Rect, command_id: u16) {}
+	fn on_command(&mut self, handle: &WindowHandle, pixel_buffer: &mut [u8], client_rect: &Rect, command_id: u16) {}
 	#[allow(unused_variables)]
-	fn on_resize(&mut self, pixel_buffer: &mut [u8], client_rect: &Rect) {}
+	fn on_timer(&mut self, handle: &WindowHandle, pixel_buffer: &mut [u8], client_rect: &Rect, timer_id: usize) {}
 	#[allow(unused_variables)]
-	fn on_resizing(&mut self, pixel_buffer: &mut [u8], client_rect: &mut Rect) {
-		self.on_resize(pixel_buffer, client_rect);
+	fn on_resize(&mut self, handle: &WindowHandle, pixel_buffer: &mut [u8], client_rect: &Rect) {}
+	#[allow(unused_variables)]
+	fn on_resizing(&mut self, handle: &WindowHandle, client_rect: &mut Rect) {}
+	#[allow(unused_variables)]
+	fn on_mouse_move(&mut self, handle: &WindowHandle, pixel_buffer: &mut [u8], client_rect: &Rect, mouse_x: i16, mouse_y: i16) {}
+	#[allow(unused_variables)]
+	fn on_mouse_left_down(&mut self, handle: &WindowHandle, pixel_buffer: &mut [u8], client_rect: &Rect, mouse_x: i16, mouse_y: i16) {}
+	#[allow(unused_variables)]
+	fn on_mouse_right_down(&mut self, handle: &WindowHandle, pixel_buffer: &mut [u8], client_rect: &Rect, mouse_x: i16, mouse_y: i16) {}
+	#[allow(unused_variables)]
+	fn on_exit(&mut self, handle: &WindowHandle) {}
+	#[allow(unused_variables)]
+	fn on_error(&mut self, handle: &WindowHandle, pixel_buffer: &mut [u8], client_rect: &Rect, error_message: &str) {}
+}
+
+
+
+pub struct WindowHandle(HWND);
+impl WindowHandle {
+	pub fn set_timer(&self, timer_id: usize, milliseconds: u32) {
+		unsafe { SetTimer(self.0, timer_id, milliseconds, None) };
 	}
-	#[allow(unused_variables)]
-	fn on_mouse_move(&mut self, pixel_buffer: &mut [u8], client_rect: &Rect, mouse_x: i16, mouse_y: i16) {}
-	#[allow(unused_variables)]
-	fn on_mouse_left_down(&mut self, pixel_buffer: &mut [u8], client_rect: &Rect, mouse_x: i16, mouse_y: i16) {}
-	#[allow(unused_variables)]
-	fn on_mouse_right_down(&mut self, pixel_buffer: &mut [u8], client_rect: &Rect, mouse_x: i16, mouse_y: i16) {}
-	#[allow(unused_variables)]
-	fn on_exit(&mut self) {}
+	pub fn request_redraw(&self) {
+		unsafe { InvalidateRect(self.0, None, false) };
+	}
 }
 
 
 struct App {
-	window_handle: HWND,
+	window_handle: WindowHandle,
 	client_rect: Rect,
 	bitmap: Option<HBITMAP>,
 	pixel_buffer: std::mem::ManuallyDrop<Box<[u8]>>,
@@ -60,8 +74,8 @@ struct App {
 unsafe extern "system" fn wnd_proc(window: HWND, message: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
 	if message == WM_NCCREATE {
 		let cs = lparam.0 as *const CREATESTRUCTW;
-		let app_ptr = (*cs).lpCreateParams as *mut c_void;
-		set_window_handle(app_ptr, window);
+		let app_ptr = (*cs).lpCreateParams as *mut App;
+		(*app_ptr).window_handle.0 = window;
 
 		SetWindowLongPtrW(window, GWLP_USERDATA, app_ptr as isize);
 	} else {
@@ -73,31 +87,21 @@ unsafe extern "system" fn wnd_proc(window: HWND, message: u32, wparam: WPARAM, l
 	DefWindowProcW(window, message, wparam, lparam)
 }
 
-fn set_window_handle(app_ptr: *mut c_void, handle: HWND) {
-	unsafe { (*(app_ptr as *mut App)).window_handle = handle; }
-}
-
 
 fn handle_message(app_ptr: *mut c_void, message: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
 	let app = unsafe { &mut *(app_ptr as *mut App) };
 	
 	match message {
 		WindowsAndMessaging::WM_TIMER => {
-			match wparam.0 {
-				1 => {
-					unsafe {
-						KillTimer(app.window_handle, 1).unwrap();
-						InvalidateRect(app.window_handle, None, false);
-					}
-				}
-				_ => {}
-			}
+			unsafe { KillTimer(app.window_handle.0, wparam.0) }.unwrap_or_else(|e| app.user_state.on_error(&app.window_handle, &mut app.pixel_buffer, &app.client_rect, &format!("Error stopping timer {}: {}", wparam.0, e)));
+			app.user_state.on_timer(&app.window_handle, &mut app.pixel_buffer, &app.client_rect, wparam.0);
 		}
-		WindowsAndMessaging::WM_SIZE | WindowsAndMessaging::WM_SIZING => {
-			let mut rect = RECT::default();
-			unsafe { GetClientRect(app.window_handle, &mut rect) }.unwrap();
-			
-			if rect.width() > 0 && rect.height() > 0 {
+		WindowsAndMessaging::WM_SIZE => {
+			let mut rect = Rect::default();
+			if let Err(e) = unsafe { GetClientRect(app.window_handle.0, &mut rect as *mut Rect as *mut RECT) } {
+				app.user_state.on_error(&app.window_handle, &mut app.pixel_buffer, &app.client_rect, &format!("Error getting window size: {e}"));
+				
+			} else if rect.width() > 0 && rect.height() > 0 {
 				app.client_rect = rect;
 				
 				let buffer_size = (app.client_rect.width() * app.client_rect.height() * 4) as usize;
@@ -125,74 +129,82 @@ fn handle_message(app_ptr: *mut c_void, message: u32, wparam: WPARAM, lparam: LP
 				
 				let mut pixel_data_pointer: *mut c_void = std::ptr::null_mut();
 				unsafe {
-					let dc = GetDC(app.window_handle);
-					app.bitmap = Some(CreateDIBSection(dc, &bmi, DIB_RGB_COLORS, &mut pixel_data_pointer, None, 0).unwrap());
-					ReleaseDC(app.window_handle, dc);
+					let dc = GetDC(app.window_handle.0);
+					app.bitmap = match CreateDIBSection(dc, &bmi, DIB_RGB_COLORS, &mut pixel_data_pointer, None, 0) {
+						Ok(bitmap) => Some(bitmap),
+						Err(e) => {
+							app.user_state.on_error(&app.window_handle, &mut app.pixel_buffer, &app.client_rect, &format!("Error reallocating bitmap: {e}"));
+							None
+						}
+					};
+					ReleaseDC(app.window_handle.0, dc);
 					app.pixel_buffer = std::mem::ManuallyDrop::new(Vec::from_raw_parts(pixel_data_pointer as *mut u8, buffer_size, buffer_size).into_boxed_slice());
 				}
+				
+				app.user_state.on_resize(&app.window_handle, &mut app.pixel_buffer, &app.client_rect);
 			}
 		}
+		WindowsAndMessaging::WM_SIZING => {
+			unsafe { app.user_state.on_resizing(&app.window_handle, &mut *(lparam.0 as *mut Rect)) };
+		}
 		WindowsAndMessaging::WM_MOUSEMOVE => {
-			app.user_state.on_mouse_move(&mut app.pixel_buffer, &app.client_rect, lparam.0 as i16, (lparam.0 >> 16) as i16);
+			app.user_state.on_mouse_move(&app.window_handle, &mut app.pixel_buffer, &app.client_rect, lparam.0 as i16, (lparam.0 >> 16) as i16);
 		}
 		WindowsAndMessaging::WM_LBUTTONDOWN => {
-			app.user_state.on_mouse_left_down(&mut app.pixel_buffer, &app.client_rect, lparam.0 as i16, (lparam.0 >> 16) as i16);
+			app.user_state.on_mouse_left_down(&app.window_handle, &mut app.pixel_buffer, &app.client_rect, lparam.0 as i16, (lparam.0 >> 16) as i16);
 		}
 		WindowsAndMessaging::WM_RBUTTONDOWN => {
-			app.user_state.on_mouse_right_down(&mut app.pixel_buffer, &app.client_rect, lparam.0 as i16, (lparam.0 >> 16) as i16);
+			app.user_state.on_mouse_right_down(&app.window_handle, &mut app.pixel_buffer, &app.client_rect, lparam.0 as i16, (lparam.0 >> 16) as i16);
 		}
 		WindowsAndMessaging::WM_COMMAND => {
-			app.user_state.on_command(&mut app.pixel_buffer, &app.client_rect, wparam.0 as u16);
+			app.user_state.on_command(&app.window_handle, &mut app.pixel_buffer, &app.client_rect, wparam.0 as u16);
 		}
 		WindowsAndMessaging::WM_PAINT => {
-			unsafe { SetTimer(app.window_handle, 1, 1000 / 60, None) };
-			
-			app.user_state.on_paint(&mut app.pixel_buffer, &app.client_rect);
+			app.user_state.on_paint(&app.window_handle, &mut app.pixel_buffer, &app.client_rect);
 			
 			unsafe {
 				let mut ps = PAINTSTRUCT::default();
-				let hdc = BeginPaint(app.window_handle, &mut ps);
+				let hdc = BeginPaint(app.window_handle.0, &mut ps);
 				
 				if let Some(bitmap) = app.bitmap {
 					let memory_dc = CreateCompatibleDC(hdc);
 					SelectObject(memory_dc, bitmap);
-					BitBlt(hdc, 0, 0, app.client_rect.width(), app.client_rect.height(), memory_dc, 0, 0, SRCCOPY).unwrap();
+					BitBlt(hdc, 0, 0, app.client_rect.width(), app.client_rect.height(), memory_dc, 0, 0, SRCCOPY).unwrap_or_else(|e| app.user_state.on_error(&app.window_handle, &mut app.pixel_buffer, &app.client_rect, &format!("Couldn't draw pixel buffer: {e}")));
 					DeleteDC(memory_dc);
 				}
 				
-				EndPaint(app.window_handle, &mut ps);
+				EndPaint(app.window_handle.0, &mut ps);
 			}
 		}
 		WindowsAndMessaging::WM_DESTROY => {
-			app.user_state.on_exit();
+			app.user_state.on_exit(&app.window_handle);
 			
 			unsafe { PostQuitMessage(0) };
 			return LRESULT(0);
 		}
 		_ => {}
 	}
-	unsafe { DefWindowProcW(app.window_handle, message, wparam, lparam) }
+	unsafe { DefWindowProcW(app.window_handle.0, message, wparam, lparam) }
 }
-
 
 
 
 pub fn run_window_process(window_id: &str, window_width: u32, window_height: u32, window_title: &str, always_on_top: bool, app_state: impl SimpleWindowApp + 'static) -> core::result::Result<i32, String> {
 	
-	unsafe { RoInitialize(RO_INIT_SINGLETHREADED) }.unwrap();
+	unsafe { RoInitialize(RO_INIT_SINGLETHREADED) }.map_err(|e| format!("Error initializing window: {e}"))?;
 	let options = DispatcherQueueOptions {
 		dwSize: std::mem::size_of::<DispatcherQueueOptions>() as u32,
 		threadType: DQTYPE_THREAD_CURRENT,
 		apartmentType: DQTAT_COM_NONE
 	};
 	
-	let controller = unsafe { CreateDispatcherQueueController(options) }.unwrap();
-	let instance = unsafe { GetModuleHandleW(None) }.unwrap();
+	let controller = unsafe { CreateDispatcherQueueController(options) }.map_err(|e| format!("Error getting queue controller: {e}"))?;
+	let instance = unsafe { GetModuleHandleW(None) }.map_err(|e| format!("Error getting module handle: {e}"))?;
 	
 	let window_id_osstr: Vec<u16> = OsStr::new(window_id).encode_wide().chain(Some(0)).collect();
 	
 	let class = WNDCLASSW {
-		hCursor: unsafe { LoadCursorW(None, IDC_ARROW) }.ok().unwrap(),
+		hCursor: unsafe { LoadCursorW(None, IDC_ARROW) }.map_err(|e| format!("Error selecting cursor: {e}"))?,
 		hInstance: instance.into(),
 		lpszClassName: PCWSTR(window_id_osstr.as_ptr()),
 		lpfnWndProc: Some(wnd_proc),
@@ -215,10 +227,10 @@ pub fn run_window_process(window_id: &str, window_width: u32, window_height: u32
 		right: window_width as i32,
 		bottom: window_height as i32,
 	};
-	unsafe { AdjustWindowRectEx(&mut adjust_rect, window_style, false, window_ex_style) }.unwrap();
+	unsafe { AdjustWindowRectEx(&mut adjust_rect, window_style, false, window_ex_style) }.map_err(|e| format!("Error adjusting window area: {e}"))?;
 	
 	let mut app = App {
-		window_handle: HWND(0),
+		window_handle: WindowHandle(HWND(0)),
 		client_rect: Rect::default(),
 		bitmap: None,
 		pixel_buffer: core::mem::ManuallyDrop::default(),
@@ -241,7 +253,7 @@ pub fn run_window_process(window_id: &str, window_width: u32, window_height: u32
 	) };
 	
 	if window.0 == 0 {
-		panic!("{}", Error::from_win32());
+		return Err(format!("Error creating window: {}", Error::from_win32()));
 	}
 	
 	/*unsafe {
@@ -274,13 +286,13 @@ pub fn run_window_process(window_id: &str, window_width: u32, window_height: u32
 	
 	
 	
-	let async_action = controller.ShutdownQueueAsync().unwrap();
+	let async_action = controller.ShutdownQueueAsync().map_err(|e| format!("Error sending shutdown signal: {e}"))?;
 	async_action.SetCompleted(&AsyncActionCompletedHandler::new(
 		move |_, _| -> Result<()> {
 			unsafe { PostQuitMessage(message.wParam.0 as i32) };
 			Ok(())
 		}
-	)).unwrap();
+	)).map_err(|e| format!("Error shutting down window: {e}"))?;
 	
 	let mut message = MSG::default();
 	unsafe {
--- src/tests.rs
+++ src/tests.rs
@@ -1,31 +1,60 @@
 #[cfg(test)]
 
 use super::*;
-use crate::{SimpleWindowApp, Rect};
+use crate::{SimpleWindowApp, WindowHandle, Rect};
 
 
 struct MyAppState {
+	previous_frame_time: std::time::Instant,
 	time: f64
 }
 
 impl SimpleWindowApp for MyAppState {
-	fn on_paint(&mut self, pixel_buffer: &mut [u8], client_rect: &Rect) {
+	fn on_paint(&mut self, handle: &WindowHandle, pixel_buffer: &mut [u8], client_rect: &Rect) {
+		handle.set_timer(0, 100);
 		
+		let now = std::time::Instant::now();
+		let dt = now.duration_since(self.previous_frame_time).as_secs_f64();
+		self.previous_frame_time = now;
+		self.time += dt;
+		
+		let width = client_rect.width() as u32;
+		let height = client_rect.height() as u32;
+		
+		for y in 0..height {
+			for x in 0..width {
+				let i = 4 * (y * width + x) as usize;
+				pixel_buffer[i] = (127 + (self.time * 60.0) as u32) as u8;
+				pixel_buffer[i + 1] = (y * 255 / height + (self.time * 60.0) as u32) as u8;
+				pixel_buffer[i + 2] = (x * 255 / width + (self.time * 60.0) as u32) as u8;
+			}
+		}
 	}
-	fn on_command(&mut self, pixel_buffer: &mut [u8], client_rect: &Rect, command_id: u16) {
+	fn on_timer(&mut self, handle: &WindowHandle, _pixel_buffer: &mut [u8], _client_rect: &Rect, _timer_id: usize) {
+		handle.request_redraw();
+	}
+	fn on_command(&mut self, _handle: &WindowHandle, _pixel_buffer: &mut [u8], _client_rect: &Rect, command_id: u16) {
 		println!("{}", command_id);
 	}
-	fn on_mouse_left_down(&mut self, pixel_buffer: &mut [u8], client_rect: &Rect, mouse_x: i16, mouse_y: i16) {
+	fn on_mouse_left_down(&mut self, _handle: &WindowHandle, _pixel_buffer: &mut [u8], _client_rect: &Rect, _mouse_x: i16, _mouse_y: i16) {
 		println!("Left click");
 	}
-	fn on_mouse_right_down(&mut self, pixel_buffer: &mut [u8], client_rect: &Rect, mouse_x: i16, mouse_y: i16) {
+	fn on_mouse_right_down(&mut self, _handle: &WindowHandle, _pixel_buffer: &mut [u8], _client_rect: &Rect, _mouse_x: i16, _mouse_y: i16) {
 		println!("Right click");
+	}
+	//fn on_resizing(&mut self, _handle: &WindowHandle, client_rect: &mut Rect) {}
+	fn on_error(&mut self, _handle: &WindowHandle, _pixel_buffer: &mut [u8], _client_rect: &Rect, error_message: &str) {
+		println!("{error_message}");
 	}
 }
 
 #[test]
 fn it_works() {
 	
-	run_window_process("a", 800, 600, "title yes", false, MyAppState { time: 0.0 }).unwrap();
+	let result = run_window_process("a", 800, 600, "title yes", false, MyAppState { previous_frame_time: std::time::Instant::now(), time: 0.0 });
+	
+	if let Err(message) = result {
+		println!("{message}");
+	}
 	
 }
